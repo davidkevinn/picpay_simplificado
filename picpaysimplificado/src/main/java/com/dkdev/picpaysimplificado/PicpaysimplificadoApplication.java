295551a9d4969fdac649e077c59dbc5fe8d3f8e6
david <davidkevin987@outlook.com>
2023-10-01 00:00:48 -0300

    feat: initial configs

---
+++ picpaysimplificado/src/main/java/com/dkdev/picpaysimplificado/PicpaysimplificadoApplication.java
@@ -0,0 +1,17 @@
+package com.dkdev.picpaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PicpaysimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimplificadoApplication.class, args);
+	}
+
+
+
+}
